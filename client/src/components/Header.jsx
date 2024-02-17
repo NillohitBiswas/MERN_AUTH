6f00d8f96593393f2455de287f2a8c156891766a
Nillohit Biswas <nillohitb1996@gmail.com>
2024-02-18 02:33:44 +0530

    create header components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div className='bg-orange-100 mt-2'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-2'>
+        <Link to='/'>   
+        <h1 className='font-authappfont font-bold text-3xl'>Auth App </h1>
+        </Link>
+        <ul className='font-typofont text-lg flex gap-4 font-semibold'>
+         <Link to='/'>
+         <li>Home</li>
+         </Link>
+         <Link to='/about'>
+         <li>About</li>
+         </Link>
+         <Link to='/sign-in'>
+         <li>Sign In</li>
+         </Link>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
