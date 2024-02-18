3cc8fedd30fb34520e79c8349bae7c2ae4ad1684
Nillohit Biswas <nillohitb1996@gmail.com>
2024-02-18 23:16:25 +0530

    connect to monogoDB

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom'
 export default function Header() {
   return (
     <div className='bg-orange-100 mt-2'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-2'>
+      <div className='flex justify-between  items-center max-w-6xl mx-auto p-2'>
         <Link to='/'>   
-        <h1 className='font-authappfont font-bold text-3xl'>Auth App </h1>
+        <h1 className='font-authappfont font-bold text-3xl justify-items-start'>Auth App </h1>
         </Link>
         <ul className='font-typofont text-lg flex gap-4 font-semibold'>
          <Link to='/'>
