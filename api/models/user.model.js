c591c619c1ee7a092d4645d21ccd3004eb9dda6c
Nillohit Biswas <nillohitb1996@gmail.com>
2024-02-19 02:14:19 +0530

    create user modlel

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userschema= new mongoose.Schema({
+    username:{
+        type :String,
+        required:true,
+        unique: true,
+    },
+    email:{
+        type :String,
+        required:true,
+        unique: true,
+    },
+    password:{
+        type :String,
+        required:true,
+    },
+    dateofbirth:{
+        type :String,
+        required:true,
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User',userschema);
+export default User;
