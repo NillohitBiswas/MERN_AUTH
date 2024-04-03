e9a903aadad09882846c1592d107c25b21a7ae82
Nillohit Biswas <nillohitb1996@gmail.com>
2024-04-03 16:38:06 +0530

    Create login api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import {errorHandler} from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
      const { username, email, password } =req.body;
@@ -16,3 +18,26 @@ export const signup = async (req, res, next) => {
         next(error);
      }
 };
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validuser = await User.findOne({ email });
+      if (!validuser) {
+        return next(errorHandler(404, "Invalid Credentials" ));
+      }
+      const validpasword = bcryptjs.compareSync(password, validuser.password);
+      if (!validpasword) {
+        return next(errorHandler(401, "Invalid User" ));
+      }
+      const token = jwt.sign({id: validuser._id},process.env.JWT_SECRET_KEY);
+      const { password: hashedPassword, ...rest} = validuser._doc;
+      const expiryDate = new Date(Date.now() + 3600000);
+      res
+      .cookie('access_token', token,{ httpOnly: true, expires: expiryDate})
+      .status(200)
+      .json( rest )
+    } catch (error) {
+      next(error);
+    }
+}
